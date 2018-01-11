f40befc7808e2d51a2c335a5809638ad22cd3532
Valentin Roland <valentin@vroland.de>
2018-01-11 22:09:08 +0100

    completed section inclusion transformation

--- src/transformations.rs
+++ src/transformations.rs
@@ -125,18 +125,62 @@ pub fn normalize_template_title(mut root: Element, settings: &Settings) -> TResu
 }
 
 
-pub fn include_sections(mut root: Element, settings: &Settings) -> TResult {
-    if let &mut Element::Template { ref name, ref content, ref position } = &mut root {
-        let prefix = &settings.deps_settings.section_inclusion_prefix;
-        let template_name = extract_plain_text(&name);
+pub fn include_sections(
+    mut root: Element,
+    settings: &Settings) -> TResult {
+    root = recurse_inplace_template(&include_sections, root, settings, &include_sections_vec)?;
+    Ok(root)
+}
 
-        // section transclusion
-        if template_name.to_lowercase().starts_with(prefix) {
-            let article = trim_prefix(&template_name, prefix);
-            if content.len() < 1 {
-                return Err(TransformationError {
-                    cause: "A section inclusion must specify article \
-                            name and section name!".to_string(),
+pub fn include_sections_vec<'a>(
+    trans: &TFuncInplace<&'a Settings>,
+    root_content: &mut Vec<Element>,
+    settings: &'a Settings) -> TListResult {
+
+    // search for section inclusion in children
+    let mut result = vec![];
+    for mut child in root_content.drain(..) {
+
+        if let &mut Element::Template {
+            ref name,
+            ref content,
+            ref position
+        } = &mut child {
+
+            let prefix = &settings.deps_settings.section_inclusion_prefix;
+            let template_name = extract_plain_text(&name);
+
+            // section transclusion
+            if template_name.to_lowercase().trim().starts_with(prefix) {
+                let article = trim_prefix(template_name.trim(), prefix);
+                if content.len() < 1 {
+                    return Err(TransformationError {
+                        cause: "A section inclusion must specify article \
+                                name and section name!".to_string(),
+                        position: position.clone(),
+                        transformation_name: "include_sections".to_string(),
+                        tree: Element::Template {
+                            name: name.clone(),
+                            position: position.clone(),
+                            content: content.clone(),
+                        }
+                    });
+                }
+
+                let section_name = extract_plain_text(content);
+                let mut section_file = settings.deps_settings.section_rev.clone();
+                section_file.push('.');
+                section_file.push_str(&settings.deps_settings.section_ext);
+
+                let path = path::Path::new(&settings.deps_settings.section_path)
+                    .join(&filename_to_make(&article))
+                    .join(&filename_to_make(&section_name))
+                    .join(&filename_to_make(&section_file));
+
+                // error returned when the section file is faulty
+                let file_error = TransformationError {
+                    cause: format!("section file `{}` could not be read or parsed!",
+                                &path.to_string_lossy()),
                     position: position.clone(),
                     transformation_name: "include_sections".to_string(),
                     tree: Element::Template {
@@ -144,52 +188,37 @@ pub fn include_sections(mut root: Element, settings: &Settings) -> TResult {
                         position: position.clone(),
                         content: content.clone(),
                     }
-                })
-            }
+                };
 
-            let section_name = extract_plain_text(content);
-            let mut section_file = settings.deps_settings.section_rev.clone();
-            section_file.push('.');
-            section_file.push_str(&settings.deps_settings.section_ext);
-
-            let path = path::Path::new(&settings.deps_settings.section_path)
-                .join(&filename_to_make(&article))
-                .join(&filename_to_make(&section_name))
-                .join(&filename_to_make(&section_file));
-
-            // error returned when the section file is faulty
-            let file_error = TransformationError {
-                cause: format!("section file `{}` could not be read or parsed!",
-                               &path.to_string_lossy()),
-                position: position.clone(),
-                transformation_name: "include_sections".to_string(),
-                tree: Element::Template {
-                    name: name.clone(),
-                    position: position.clone(),
-                    content: content.clone(),
+                let section_str = File::open(&path);
+                if section_str.is_err() {
+                    return Err(file_error)
                 }
-            };
 
-            let section_str = File::open(&path);
-            if section_str.is_err() {
-                return Err(file_error)
+                let mut section_tree: Vec<Element>
+                    = match serde_yaml::from_reader(&section_str.unwrap()) {
+                    Ok(root) => root,
+                    Err(_) => return Err(file_error),
+                };
+
+                result.push(
+                    Element::Comment {
+                        position: position.clone(),
+                        text: format!("included from: {}|{}", article, section_name),
+                    }
+                );
+
+                // recursively include sections
+                section_tree = include_sections_vec(
+                    &include_sections,
+                    &mut section_tree,
+                    settings,
+                )?;
+                result.append(&mut section_tree);
+                continue
             }
-
-            let section_tree: Vec<Element>
-                 = match serde_yaml::from_reader(&section_str.unwrap()) {
-                Ok(root) => root,
-                Err(_) => return Err(file_error)
-            };
-
-            eprintln!("include article: {:?}", path);
-
-            return Ok(
-                Element::Comment {
-                    position: position.clone(),
-                    text: format!("#lst:{}|{}", article, section_name),
-                }
-            );
         }
+        result.push(trans(child, settings)?);
     }
-    recurse_inplace(&include_sections, root, settings)
+    Ok(result)
 }
