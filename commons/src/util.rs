10f60b8e9e6c173ba4856698e85ee4cc81376ea1
Valentin Roland <valentin@vroland.de>
2018-04-17 22:53:27 +0200

    add cached texvccheck function

--- commons/src/util.rs
+++ commons/src/util.rs
@@ -1,6 +1,8 @@
 //! Common utilities for mfnf tools.
 
 use mediawiki_parser::*;
+use std::collections::HashMap;
+use std::process::Command;
 
 /// generates getters and setters for a path member of a traversion.
 #[macro_export]
@@ -39,6 +41,79 @@ pub fn extract_plain_text(content: &[Element]) -> String {
         };
     }
     result
+}
+
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
+pub enum TexResult {
+    Ok(String),
+    UnknownFunction(String),
+    LexingError,
+    SyntaxError,
+    UnknownError,
+}
+
+pub trait TexChecker {
+    fn check(&mut self, source: &str) -> TexResult;
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct CachedTexChecker {
+    pub texvccheck_path: String,
+    pub max_size: usize,
+    pub cache: HashMap<String, TexResult>,
+}
+
+impl CachedTexChecker {
+    pub fn new(path: &str, size: usize) -> CachedTexChecker {
+        CachedTexChecker {
+            texvccheck_path: path.into(),
+            max_size: size,
+            cache: HashMap::with_capacity(size),
+        }
+    }
+
+    pub fn set_path(&mut self, path: &str) {
+        self.texvccheck_path = path.into();
+    }
+
+    pub fn get_path(&self) -> &str {
+        &self.texvccheck_path
+    }
+}
+
+impl TexChecker for CachedTexChecker {
+    fn check(&mut self, source: &str) -> TexResult {
+        if let Some(result) = self.cache.get(source) {
+            return result.clone()
+        }
+
+        let mut output = Command::new(&self.texvccheck_path).arg(source).output()
+            .expect("Failed to launch texvccheck!");
+        let mut iter = output.stdout.drain(..);
+        let first = iter.next();
+        let text = String::from_utf8(iter.collect())
+            .expect("Corrupted texvccheck output!");
+        let result = match first {
+            Some(c) => match c as char {
+                '+' => TexResult::Ok(text),
+                'F' => TexResult::UnknownFunction(text),
+                'S' => TexResult::SyntaxError,
+                'E' => TexResult::LexingError,
+                _ => TexResult::UnknownError,
+            },
+            _ => TexResult::UnknownError,
+        };
+
+        if self.cache.len() > self.max_size {
+            let mut count = 0;
+            self.cache.retain(|_, _| {
+                count += 1;
+                count % 10 != 1
+            });
+        }
+        self.cache.insert(source.into(), result.clone());
+        result
+    }
 }
 
 /// Returns the template argument with a matching name (lowercase) from a list.
