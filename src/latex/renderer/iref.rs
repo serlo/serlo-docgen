b6fb952c734340bfecbbef77614bcc130b5c64d5
Valentin Roland <valentin@vroland.de>
2018-07-15 21:00:17 +0200

    refactored license text rendering

--- src/latex/renderer/iref.rs
+++ src/latex/renderer/iref.rs
@@ -5,6 +5,35 @@ use preamble::*;
 use std::path;
 
 impl<'e, 's: 'e, 't: 'e> LatexRenderer<'e, 't> {
+
+    pub fn get_license_text(
+        &mut self,
+        root: &'e InternalReference,
+        settings: &'s Settings,
+        out: &mut io::Write
+    ) -> io::Result<Option<String>> {
+        let meta = match load_media_meta(&root.target, settings) {
+            MetaLoadResult::Meta(m) => m,
+            e @ _ => {
+                let error = escape_latex(&format!("{:#?}", e));
+                self.write_error(&error, out)?;
+                return Ok(None);
+            }
+        };
+        let authors = meta.license.authors.join(", ");
+        let license_text = format!(LICENSE_TEXT!(),
+            &escape_latex(&meta.license.url),
+            &escape_latex(&path::PathBuf::from(&meta.license.url)
+                .file_name()
+                .map(|f| f.to_string_lossy())
+                .unwrap_or_default()
+            ),
+            &escape_latex(&authors),
+            &escape_latex(&meta.license.shortname),
+        );
+        Ok(Some(license_text))
+    }
+
     pub fn internal_ref(
         &mut self,
         root: &'e InternalReference,
@@ -19,25 +48,10 @@ impl<'e, 's: 'e, 't: 'e> LatexRenderer<'e, 't> {
         // embedded files (images, videos, ...)
         if is_file(root, settings) {
             let image_path = build_image_path(self.latex, &root.target, settings);
-            let meta = match load_media_meta(&root.target, settings) {
-                MetaLoadResult::Meta(m) => m,
-                e @ _ => {
-                    let error = escape_latex(&format!("{:#?}", e));
-                    self.write_error(&error, out)?;
-                    return Ok(false);
-                }
+            let license_text = match self.get_license_text(root, settings, out)? {
+                Some(s) => s,
+                None => return Ok(false)
             };
-            let authors = meta.license.authors.join(", ");
-            let license_text = format!(LICENSE_TEXT!(),
-                &escape_latex(&meta.license.url),
-                &escape_latex(&build_file_path(&root.target, settings)
-                    .file_name()
-                    .map(|f| f.to_string_lossy())
-                    .unwrap_or_default()
-                ),
-                &escape_latex(&authors),
-                &escape_latex(&meta.license.shortname),
-            );
 
             // collect image options
             let mut image_options = vec![];
