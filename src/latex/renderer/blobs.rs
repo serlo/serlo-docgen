dc533fc7a4e7ac1e6668b3abcc9e2b75424b257f
Valentin Roland <valentin@vroland.de>
2018-10-06 19:28:00 +0200

    prevent double * for math and important

--- src/latex/renderer/blobs.rs
+++ src/latex/renderer/blobs.rs
@@ -93,8 +93,8 @@ alias!(QUOTE_ENV, "displayquote");
 
 // --- Templates ---
 
-alias!(MATH_ENV, "align*");
-alias!(IMPORTANT_ENV, "important*");
+alias!(MATH_ENV, "align");
+alias!(IMPORTANT_ENV, "important");
 alias!(PROOF_STEP_CAPTION, "\\textbf{{{}}}: {}:\n");
 alias!(PROOF_CASE_CAPTION, "\\textbf{{{} {}}}: {}:\n");
 alias!(
